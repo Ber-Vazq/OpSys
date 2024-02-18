6388eab04313aec01504d7dcb3dc0a15a180de6c
Ber-Vazq <bvaq20@hotmail.com>
2024-02-18 15:11:26 -0600

    readme ready

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # OpSys
 anything and everything for OS
+
+Project 1:
+-Group:
+--   Bernardo Vazquez(jkg215)
+
+-Files and Directories:
+--
+
+-Compiling this program:
+--
+
+-Running this program:
+--
+
+-Challenges:
+--
+
+-Sources:
+--
---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+Requirements for Project 1:
+-Prompt the user
+--make prompt different color from command
+-able to parse commands from command line
+-execute commands
+-built ins
+---Exit
+---cd{PATH}
+---showpid()
+*/
+
+#include <stdio.h>;
+
+
+
