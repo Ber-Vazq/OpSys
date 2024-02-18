0c43a646ce81bad6b2f2e4856ed8b918f6529393
Ber-Vazq <bvaq20@hotmail.com>
2024-02-18 15:39:31 -0600

    getting some shit down

--- main.c
+++ main.c
@@ -6,11 +6,32 @@ Requirements for Project 1:
 -execute commands
 -built ins
 ---Exit
----cd{PATH}
+---cd[PATH]
 ---showpid()
 */
 
 #include <stdio.h>;
+#include <stdlib.h>;
+#include <string.h>;
 
 
+int main(int argc, char **argv){
+    char str[1000];
 
+    while(1){
+        //this is so we can read the input from user
+        if(fgets(str, sizeof(str), stdin)!= NULL){
+            fprintf(stderr, "Error reading input, please fix.\n");
+            break;//exit if error
+        }
+
+        str[strcspn(str, "\n")] = '\0';//remove traling newline if there
+
+        if (strncmp(str, "exit",4)==0)
+        {
+            printf("EXITING!\n");
+            break;
+        }
+        
+    }
+}
